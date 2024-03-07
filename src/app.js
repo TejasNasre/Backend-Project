43ed2dbc7675a0c44b8828ad50d6030dcf3a270e
tejasnasre <tejasnasre120@gmail.com>
2024-03-07 19:36:01 +0530

    Update Utils For Reusable Error And Response Handelling

--- src/app.js
+++ src/app.js
@@ -9,11 +9,7 @@ app.use(
     credentials: true,
   })
 );
-app.use(
-  express.json({
-    limit: "16kb",
-  })
-);
+app.use(express.json({ limit: "16kb" }));
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,31 @@
 const asyncHandler = (fn) => async (req, res) => {
-    try {
-        await fn(req, res,next);
-    } catch (error) {
-        res.status(500).json({ success : false ,message: error.message });
-    }
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 export default asyncHandler;
+
+/*
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch(err => next(err);
+    }
+}
+
+Another Method For Writting The AsyncHandler Function In JavaScript
+*/
+
+/*
+By Use Of Promise.resolve() Method
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch(err => next(err);
+    }  
+}
+
+Another Method For Writting The AsyncHandler Function In JavaScript
+*/
